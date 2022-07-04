5758a1b927fecc6d602df613870aa0e77bec2fd1
Carllass <Evilangel3211@gmail.com>
2022-07-05 01:10:45 +0500

    added jwt code

--- config.py
+++ config.py
@@ -3,3 +3,6 @@ from starlette.config import Config
 
 config = Config('.env')
 DATABASE_URL = config('DATABASE_URL', cast=str, default='')
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ALGORITHM = 'HS256'
+SECRET_KEY = config('SECRET_KEY', cast=str, default='ab9be59a91808afb')
